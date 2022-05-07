253860eb7f0c58751ce6e310284597a18a16bf74
Roneel Valambhia <roneelmv@gmail.com>
2022-05-08 04:18:50 +0530

    feat: show interactive map with leaflet
    complete homomorphic logic for base proximity detection
    complete map interface including draggable marker

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,113 +1,195 @@
-import axios from 'axios';
-import moment from 'moment';
-import { generateRandomKeys } from 'paillier-bigint';
-import JSONbig from 'json-bigint'
-import React, { useState } from 'react'
+import { format } from "date-fns";
+import { generateRandomKeys } from "paillier-bigint";
+import JSONbigint from "json-bigint";
+import { useState, useRef, useMemo } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Rectangle,
+} from "react-leaflet";
 
-function Form({ addCheckPoint }) {
-	const [open, setOpen] = useState(true);
-	const [x, setX] = useState("");
-	const [y, setY] = useState("");
+let JSONbig = JSONbigint({
+  alwaysParseAsBig: true,
+  useNativeBigInt: true,
+});
 
-	async function handle_submit(e) {
-		e.preventDefault();
+const keyLength = 512;
+const latMultiplier = 1e15;
+const randMultiplier = 1e9; // used in only the server
+const maxLength = Math.log10(latMultiplier) + Math.log10(randMultiplier);
 
-		if (!isNaN(x) && !isNaN(y)) {
+/** utility function for paillier decrypted bigints
+ * modulo arithmetic results in negative numbers being decrypted as large bigints
+ * 25/maxLength is the maximum length an actually positive decrypted bigint can have
+ * which depends on the implementation details
+ * @param {bigint} x
+ * @returns number
+ */
+const bigintSign = (x) => {
+  return Math.sign(x.toString().length - maxLength);
+};
 
-			// take input log 1
-			addCheckPoint({
-				tag: "Client",
-				title: "Input",
-				log: "Input location",
-				time: moment(new Date()).format("LLL").toString(),
-			});
-			// setOpen(false);
+async function handle_submit(e, addCheckPoint, lat, lng) {
+  e.preventDefault();
 
-			const { publicKey, privateKey } = await generateRandomKeys(1024);
+  // take input log 1
+  addCheckPoint({
+    tag: "Client",
+    title: "Input",
+    log: "Input location",
+    time: format(new Date(), "PPPp"),
+  });
 
-			const locx = publicKey.encrypt(parseInt(x));
-			const locy = publicKey.encrypt(parseInt(y));
+  const { publicKey, privateKey } = await generateRandomKeys(keyLength);
 
-			// encryption log 2
-			addCheckPoint({
-				tag: "Client",
-				title: "Encryption",
-				log: "Data encrypted",
-				time: moment(new Date()).format("LLL").toString(),
-			});
+  // convert to integers and then cipher
+  const ilat = Math.ceil(lat * latMultiplier);
+  const ilng = Math.ceil(lng * latMultiplier);
 
+  const clat = publicKey.encrypt(ilat);
+  const clng = publicKey.encrypt(ilng);
 
-			const reqdata = JSONbig.stringify({
-				x: locx,
-				y: locy,
-				n: publicKey.n,
-				g: publicKey.g,
-			});
+  // encryption log 2
+  addCheckPoint({
+    tag: "Client",
+    title: "Encryption",
+    log: "Data encrypted",
+    time: format(new Date(), "PPPp"),
+  });
 
-			// server log 3
-			const res = await axios.post("http://localhost:5000/location", { reqdata })
-				.catch(err => {
-					console.log(err);
-					// error handling clear timeline and reset form
-				})
+  let reqdata = JSONbig.stringify({
+    lat: clat,
+    lng: clng,
+    n: publicKey.n,
+    g: publicKey.g,
+  });
 
-			console.log(res);
-			// const offset = JSONbig.parse(res.data.offsetStr);
-			// log 4
-			addCheckPoint({
-				tag: "Server",
-				title: "Calculation",
-				log: "Server calculation completed",
-				time: moment(new Date()).format("LLL").toString(),
-			});
+  // server log 3
 
+  let res = await fetch(`${process.env.REACT_APP_SERVER_URL}/locate`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({ payload: reqdata }),
+  });
+  let data = await res.json();
+  // console.log(data);
+  // console.log(JSONbig.parse(data.offsetStr));
+  let offsets = JSONbig.parse(data.offsetStr);
+  // log 4
+  addCheckPoint({
+    tag: "Server",
+    title: "Calculation",
+    log: "Server calculation completed",
+    time: format(new Date(), "PPPp"),
+  });
 
+  let plainOffsets = offsets.map(({ name, lat1, lat2, lng1, lng2 }) => ({
+    name,
+    lat1: privateKey.decrypt(lat1),
+    lat2: privateKey.decrypt(lat2),
+    lng1: privateKey.decrypt(lng1),
+    lng2: privateKey.decrypt(lng2),
+  }));
 
-
-		}
-		else {
-			alert("Enter Valid location");
-		}
-	}
-
-	return (
-		<div className={'w-full h-full grid place-content-center ' + (open ? "" : "hide")}>
-			<form
-				className='p-8 flex flex-col items-center gap-y-5 shadow-md bg-white rounded'
-				onSubmit={handle_submit}
-			>
-				<h1 className='text-blue-500'>
-					Find Your Location
-				</h1>
-				<input
-					className='border rounded border-gray-300'
-					type={'number'}
-					placeholder="Location X"
-					id="locx"
-					min={-400}
-					max={400}
-					value={x}
-					onChange={(e) => { setX(e.target.value) }}
-				/>
-				<input
-					className='border rounded border-gray-300'
-					type={'number'}
-					placeholder="Location X"
-					id="locx"
-					min={-400}
-					max={400}
-					value={y}
-					onChange={(e) => { setY(e.target.value) }}
-				/>
-				<button
-					className='rounded px-6 py-2 bg-blue-400 text-white'
-					type='submit'
-				>
-					Find Geo Fence
-				</button>
-			</form>
-		</div>
-	)
+  plainOffsets.forEach(({ name, lat1, lat2, lng1, lng2 }) => {
+    console.log(lat1, lat2, lng1, lng2);
+    if (
+      bigintSign(lat1) != bigintSign(lat2) &&
+      bigintSign(lng1) != bigintSign(lng2)
+    ) {
+      alert(`You are in proximity of ${name}.`);
+      console.log(name);
+      return { name };
+    }
+  });
+  console.log("none found");
 }
 
-export default Form
+// Saint Petersburg
+let center = {
+  lng: 30.355911254882814,
+  lat: 59.94634865982191,
+};
+
+let outerBounds = [
+  { lat: 59.88032391089786, lng: 30.222015380859375 },
+  { lat: 60.01237340874596, lng: 30.489807128906254 },
+];
+
+/**
+ * @param {{lat: number, lng:number}} pos
+ * @param {[{lat: number, lng:number}]} bounds
+ * @returns boolean
+ */
+const isPosInBounds = (pos, bounds) => {
+  if (
+    Math.sign(pos.lat - bounds[0].lat) == Math.sign(pos.lat - bounds[1].lat) ||
+    Math.sign(pos.lng - bounds[0].lng) == Math.sign(pos.lng - bounds[1].lng)
+  )
+    return false;
+  return true;
+};
+
+function Form({ addCheckPoint, startTimeline }) {
+  const [position, setPosition] = useState(center);
+  const markerRef = useRef(null);
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current;
+        if (marker != null) {
+          let pos = marker.getLatLng();
+          if (isPosInBounds(pos, outerBounds)) {
+            setPosition(marker.getLatLng());
+          } else {
+            marker.position = center;
+            setPosition(center);
+            alert("set marker inside the bound rectangle");
+          }
+        }
+      },
+    }),
+    []
+  );
+
+  return (
+    <div>
+      <div id="map">
+        <MapContainer bounds={outerBounds} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; Mapbox | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXl0ZW1wdXNlciIsImEiOiJjbDJ2ajFkdnUwNHhpM2Rub3EyMmZnd3ZxIn0.8DPFWlLRBMbBx008IOeQwQ"
+            id="mapbox/streets-v11"
+          />
+          <Marker
+            draggable={true}
+            eventHandlers={eventHandlers}
+            position={position}
+            ref={markerRef}
+          >
+            <Popup>Drag the marker to your position and submit.</Popup>
+          </Marker>
+          <Rectangle bounds={outerBounds} pathOptions={{ color: "#cccccc" }} />
+        </MapContainer>
+      </div>
+      <div>
+        <button
+          type="submit"
+          onClick={(e) => {
+            startTimeline();
+            handle_submit(e, addCheckPoint, position.lat, position.lng);
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
--- src/Components/Timeline.jsx
+++ src/Components/Timeline.jsx
@@ -1,52 +1,23 @@
-import React from 'react'
-
-// const timelineData = [
-// 	{
-// 		tag: 'client',
-// 		title: 'Client Log',
-// 		log: 'Started working on the app-ideas repository',
-// 		time: 'February 25 2019',
-// 	},
-// 	{
-// 		tag: 'client',
-// 		title: 'Client Log',
-// 		log: 'Started working on the app-ideas repository',
-// 		time: 'February 25 2019',
-// 	},
-// 	{
-// 		tag: 'server',
-// 		title: 'Server Log',
-// 		log: 'Started working on the app-ideas repository',
-// 		time: 'February 25 2019',
-// 	},
-// 	{
-// 		tag: 'server',
-// 		title: 'Server Log',
-// 		log: 'Started working on the app-ideas repository',
-// 		time: 'February 25 2019',
-// 	},
-// ]
-
 const TimelineItem = ({ data }) => (
-	<>
-		<div className={"timeline-item " + data.tag}>
-			<div className="timeline-item-content">
-				<time>{data.time}</time>
-				<h1>{data.title}</h1>
-				<p>{data.log}</p>
-				<span className="circle" />
-			</div>
-		</div>
-	</>
+  <>
+    <div className={"timeline-item " + data.tag}>
+      <div className="timeline-item-content">
+        <time>{data.time}</time>
+        <h1>{data.title}</h1>
+        <p>{data.log}</p>
+        <span className="circle" />
+      </div>
+    </div>
+  </>
 );
 
 const Timeline = ({ timelineData }) =>
-	timelineData.length > 0 && (
-		<div className="timeline-container">
-			{timelineData.map((data, idx) => (
-				<TimelineItem data={data} key={idx} />
-			))}
-		</div>
-	);
+  timelineData.length > 0 && (
+    <div className="timeline-container">
+      {timelineData.map((data, idx) => (
+        <TimelineItem data={data} key={idx} />
+      ))}
+    </div>
+  );
 
-export default Timeline
+export default Timeline;
